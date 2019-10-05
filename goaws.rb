c1941ff59f8d9cfd9d97e16ded2dcc4f87dcf4b1
Paul Fortin <paul@cp-soft.com>
2019-10-05 14:58:21 -0400

    GoAws v0.2.2

--- goaws.rb
+++ goaws.rb
@@ -1,8 +1,8 @@
 class Goaws < Formula
   desc "AWS (SQS/SNS) Clone for Development testing"
-  homepage ""
-  url "https://github.com/p4tin/goaws/releases/download/v0.2.0/GoAws_0.2.0_Darwin_x86_64.tar.gz"
-  sha256 "646c2d0c855e50821cb6e705764243400d272eafe817dfc78ce774340dc6e7fa"
+  homepage "https://github.com/p4tin/goaws"
+  url "https://github.com/p4tin/goaws/releases/download/v0.2.2/GoAws_0.2.2_Darwin_x86_64.tar.gz"
+  sha256 "85f33c031a3d3a4aadcd8f600ebf4e747d046aac78a955b89d856cc91b17d11f"
 
   # depends_on "cmake" => :build
 
