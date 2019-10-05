b9e536be076cb25204081b006dcf88c72e060c7f
Paul Fortin <paul@cp-soft.com>
2019-10-05 15:10:06 -0400

    GoAws v0.2.2

--- goaws.rb
+++ goaws.rb
@@ -3,6 +3,7 @@ class Goaws < Formula
   homepage "https://github.com/p4tin/goaws"
   url "https://github.com/p4tin/goaws/releases/download/v0.2.2/GoAws_0.2.2_Darwin_x86_64.tar.gz"
   sha256 "85f33c031a3d3a4aadcd8f600ebf4e747d046aac78a955b89d856cc91b17d11f"
+  version "v0.2.2"
 
   # depends_on "cmake" => :build
 
