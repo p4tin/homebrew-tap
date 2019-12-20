638a675cbf6d74a222ff273947cd44e9161d9dbc
Paul Fortin <paul@cp-soft.com>
2019-12-20 16:20:54 -0500

    version 0.1.1

--- imperius.rb
+++ imperius.rb
@@ -1,9 +1,9 @@
 class Imperius < Formula
   desc "API command line tester"
   homepage "https://github.com/p4tin/imperius"
-  url "https://github.com/p4tin/imperius/releases/download/v0.1.0/imperius_0.1.0_Darwin_x86_64.tar.gz"
-  sha256 "1414898c83c3116d16df6fb441248db6070758056c6fdd95d4331381d0f12ac3"
-  version "v0.1.0"
+  url "https://github.com/p4tin/imperius/releases/download/v0.1.1/imperius_0.1.1_Darwin_x86_64.tar.gz"
+  sha256 "1279d4090695e73dbda5393c23dcd41b2ed27d18c99b9275a5ecace493f58e9e"
+  version "v0.1.1"
 
   # depends_on "cmake" => :build
 
