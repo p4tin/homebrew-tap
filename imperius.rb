fba4ec7fd16d0568bf33c6f8f5e388b5243ec830
Paul Fortin <paul@cp-soft.com>
2020-01-18 10:38:15 -0500

    Update imperius to version 0.3.0

--- imperius.rb
+++ imperius.rb
@@ -1,9 +1,9 @@
 class Imperius < Formula
   desc "API command line tester"
   homepage "https://github.com/p4tin/imperius"
-  url "https://github.com/p4tin/imperius/releases/download/v0.2.0/imperius_0.2.0_Darwin_x86_64.tar.gz"
-  sha256 "02625424a2320b61a32683e52b1f260a3fa14e3e63da2b211d804d1fc6e647f0"
-  version "v0.2.0"
+  url "https://github.com/p4tin/imperius/releases/download/v0.3.0/imperius_0.3.0_Darwin_x86_64.tar.gz"
+  sha256 "5651d984ff8a8e9076d001d4d02260af284b8377d254457f999821384421298e"
+  version "v0.3.0"
 
   # depends_on "cmake" => :build
 
